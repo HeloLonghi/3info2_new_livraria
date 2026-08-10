38657e832062991c4a2bd8a3d1f7f58db7b5bba2
HeloLonghi <heloisa.longhi.ifc@gmail.com>
2026-08-10 14:29:46 -0300

    FEAT: Criação de um serializador específico para a listagem de compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -9,6 +9,15 @@ class ItensCompraCreateUpdateSerializer(ModelSerializer):
         fields = ('livro', 'quantidade')
 
 
+class ItensCompraListSerializer(ModelSerializer):
+    livro = CharField(source='livro.titulo', read_only=True)
+
+    class Meta:
+        model = ItensCompra
+        fields = ('quantidade', 'livro')
+        depth = 1
+
+
 class CompraCreateUpdateSerializer(ModelSerializer):
     itens = ItensCompraCreateUpdateSerializer(many=True)
 
@@ -48,6 +57,15 @@ class ItensCompraSerializer(ModelSerializer):
         model = ItensCompra
         fields = ('titulo', 'editora', 'quantidade', 'preco', 'total', 'capa')
         depth = 1
+
+
+class CompraListSerializer(ModelSerializer):
+    usuario = CharField(source='usuario.email', read_only=True)
+    itens = ItensCompraListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Compra
+        fields = ('id', 'usuario', 'itens')
 
 
 class CompraSerializer(ModelSerializer):
